71fa238dd7f74cc4f48570aefea528e1b2a97eba
Francisco Mendoza <seecware@gmail.com>
2025-04-12 19:53:21 -0600

    NEW TABLE SCHEMA - FIT JSON INTO SQLITE3 Creating a new middleware fnc to add raw data into db, is not finished. Workaround. Need to fit json into sqlite3 some- how.

--- db_downloader_middleware.py
+++ db_downloader_middleware.py
@@ -8,4 +8,3 @@ def create_new_user(user_id, username):
 def insert_video(aweme_id, video_id, video_url, tittle, user_id):
     add_video(aweme_id, video_id, video_url, tittle, user_id)
     print(f"Inserting video data into database: {video_id} added correctly to db.")
-
